0de35039f2c0165e540cf08a6874692b6020d2a3
Andrea Evelyn Mejía Rubio <andiemartin33@gmail.com>
2021-02-27 03:07:34 -0600

    Agregar archivos de proyecto.

---
+++ plano_cartesiano/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace plano_cartesiano
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //inicializar mi grafico
+            Graphics papel = this.CreateGraphics();
+
+            //inicializamos la pluma
+            Pen lapiz = new Pen(Color.Blue, 3);
+            Pen lapizRojo = new Pen(Color.Red,2);
+
+            int X = Width / 2;  //400
+            int y = Height / 2;   //300
+
+            //trazar los ejes (dibujar)
+
+            papel.DrawLine(lapiz, X, 0, X, 600);
+            papel.DrawLine(lapiz, 0, y, 800, y);
+
+            //eje
+
+            papel.DrawLine(lapizRojo,X,y,600,150);
+        
+        
+        }
+    }
+}
